6bbf7c8350b996f50693c2434bb2407bf0650d07
trondtjemsland <69841794+trondtjemsland@users.noreply.github.com>
2022-05-26 01:04:51 +0200

    added lang in next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ module.exports = {
 	images: {
 		domains: ['netlify.app', 'preeminent-valkyrie-e01801.netlify.app'],
 	},
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
 };
 
 const nextConfig = {
